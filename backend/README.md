16fcc42ff371211facdc8f87d9c3903d7984a2b6
ADAMALAMEDDINE <99357140+ADAMALAMEDDINE@users.noreply.github.com>
2022-07-19 23:39:48 +0200

    finalcorrection

--- backend/README.md
+++ backend/README.md
@@ -11,14 +11,13 @@ touch .env
 ```
 ### .env file content must look like :
 ```
-SERVER_PORT=<port number of your choice>
+SERVER_PORT=<3001>
 DB_NAME=<same db name as the one you created in mysql>
 DB_USER=<one of your mysql accounts id>
 DB_PASS=<your mysql account password>
 DB_HOST=<your host (localhost if in local)>
 JWT_SECRET=<the scret of your choice>
 JWT_DURING=<time before session expires - ex : 1 d>
-USER_ADMIN_FIRSTNAME=<the firstname who will give admin role to user when signing in with>
 BCRYPT_SALT_ROUND=<salting for password encryption ex : 5>
 ```
 
@@ -37,4 +36,6 @@ npm run start
 ```
 npm run dev
 ```
+### For using admin user you should change the role in the db (colone : role, value : admin)
+
 
--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -13,22 +13,22 @@ exports.login = async (req, res) => {
     const { email, password } = req.body
 
     // Validation des données reçues
-    if(!email || !password){
-        return res.status(400).json({ message: 'Adresse mail ou mot de passe manquant(s)'})
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Adresse mail ou mot de passe manquant(s)' })
     }
 
-    try{
+    try {
         // Vérification si l'utilisateur existe
-        let user = await User.findOne({ where: {email: email}, raw: true})
-        if(user === null){
-            return res.status(401).json({ message: 'Ce compte n\'existe pas !'})
+        let user = await User.findOne({ where: { email: email }, raw: true })
+        if (user === null) {
+            return res.status(401).json({ message: 'Ce compte n\'existe pas !' })
         }
 
         // Vérification du mot de passe
         //let test = await bcrypt.compare(password, user.password)  
         let test = await User.checkPassword(password, user.password)
-        if(!test){
-            return res.status(403).json({ message: 'Mot de passe incorrect'})
+        if (!test) {
+            return res.status(403).json({ message: 'Mot de passe incorrect' })
         }
         // Génération du token et envoi
         const token = jwt.sign({
@@ -38,56 +38,56 @@ exports.login = async (req, res) => {
             email: maskEmail(email),
             nickname: user.nickname,
             role: user.role
-        }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_DURING})
-        
-        return res.status(200).json({token, user_id: user.id, user_role: user.role})
-    }catch(err){
-        if(err.name == 'SequelizeDatabaseError'){
+        }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_DURING })
+
+        return res.status(200).json({ token, user_id: user.id, user_role: user.role })
+    } catch (err) {
+        if (err.name == 'SequelizeDatabaseError') {
             res.status(500).json({ message: 'Une erreur inconnue est survenue', error: err })
         }
-        res.status(500).json({ message: 'Echec lors de la connexion.', error: err})        
+        res.status(500).json({ message: 'Echec lors de la connexion.', error: err })
     }
 }
 
 exports.signup = async (req, res) => {
     let { firstname, lastname, nickname, email, password } = req.body;
     // Validation des données reçues
-    if(!firstname || !lastname || !nickname || !email || !password) {
-        return res.status(400).json({ message: 'Information(s) manquante(s)'})
+    if (!firstname || !lastname || !nickname || !email || !password) {
+        return res.status(400).json({ message: 'Information(s) manquante(s)' })
     }
 
-    if(!nickname.match(/([A-Za-z0-9]{3,}$)/)) {
-        return res.status(400).json({ message: 'Le pseudonyme doit contenir au moins 3 caractères alphanumériques.'})
+    if (!nickname.match(/([A-Za-z0-9]{3,}$)/)) {
+        return res.status(400).json({ message: 'Le pseudonyme doit contenir au moins 3 caractères alphanumériques.' })
     }
 
-    if(!firstname.match(/([A-Za-z]{2,}$)/)) {
-        return res.status(400).json({ message: 'Format du prénom non-valide.'})
+    if (!firstname.match(/([A-Za-z]{2,}$)/)) {
+        return res.status(400).json({ message: 'Format du prénom non-valide.' })
     }
 
-    if(!lastname.match(/([A-Za-z]{2,}$)/)) {
-        return res.status(400).json({ message: 'Format du nom non-valide.'})
+    if (!lastname.match(/([A-Za-z]{2,}$)/)) {
+        return res.status(400).json({ message: 'Format du nom non-valide.' })
     }
 
-    if(!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
-        return res.status(400).json({ message: 'Le mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule et un chiffre.'})
+    if (!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
+        return res.status(400).json({ message: 'Le mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule et un chiffre.' })
     }
 
 
     let role = "user";
-    // if(firstname === process.env.USER_ADMIN_FIRSTNAME) (
-    //     role = "admin"
-    // )
+    /*if (firstname === process.env.USER_ADMIN_FIRSTNAME) (
+        role = "admin"
+    )*/
 
     password = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUND));
 
 
-    try{
+    try {
         //création du User
         const user = await User.create({ firstname, lastname, nickname, email, password, role })
         return res.status(200).json({ message: 'Utilisateur créé !', data: user })
-    }catch(err){
+    } catch (err) {
         let message = 'Une erreur inconnue est survenue';
-        if(err.original.code === "ER_DUP_ENTRY") {
+        if (err.original.code === "ER_DUP_ENTRY") {
             message = "Le pseudo " + nickname + " est déjà pris !";
         }
         return res.status(400).json({ message, error: err })
@@ -102,8 +102,8 @@ const maskEmail = emailStr => {
 
 const obfuscate = str => {
     let obfuscated = "";
-    for(let i = 0, l =  str.length; i < l; i++) {
-        if(i < str.length / 2) {
+    for (let i = 0, l = str.length; i < l; i++) {
+        if (i < str.length / 2) {
             obfuscated += "*";
         } else {
             obfuscated += str[i];
