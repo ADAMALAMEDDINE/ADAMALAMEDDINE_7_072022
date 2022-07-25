6ab7ab1a9d6694d766de54b045e53c13abfd10b7
ADAMALAMEDDINE <99357140+ADAMALAMEDDINE@users.noreply.github.com>
2022-07-25 12:20:45 +0200

    finale

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,6 @@ require("dotenv").config();
 /*** Import de la connexion à la DB */
 let DB = require('./db.config');
 
-
-
 /*****************************/
 /*** Initialisation de l'API */
 const app = express();
